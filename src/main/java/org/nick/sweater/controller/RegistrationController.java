5cd854c8791706724d3c0661558dc153696a0d87
DeliriumTraemens <oskinnick@gmail.com>
2021-10-15 11:39:09 +0300

    Mysql Working. Just Messages. Security Added. Author of Message Added Picture_Uploading Working login.html remastered message list remastered Mail Dependency added User fields added Mail Settings and Service Added

--- src/main/java/org/nick/sweater/controller/RegistrationController.java
+++ src/main/java/org/nick/sweater/controller/RegistrationController.java
@@ -3,38 +3,70 @@ package org.nick.sweater.controller;
 import org.nick.sweater.domain.Role;
 import org.nick.sweater.domain.User;
 import org.nick.sweater.repos.UserRepo;
+import org.nick.sweater.service.MailSender;
+import org.nick.sweater.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collections;
+import java.util.UUID;
 
 @Controller
 public class RegistrationController {
 	@Autowired
 	UserRepo userRepo;
 	
+	@Autowired
+	private MailSender mailSender;
+	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping("/registration")
-		public String registrationPage(){
+	public String registrationPage() {
 		return "registration";
 	}
 	
 	@PostMapping("registration")
-	public String addNewUser(@RequestParam String username, @RequestParam String password, Model model){
+	public String addNewUser(@RequestParam String username, @RequestParam String password, Model model) {
 //		User userFromDB=userRepo.findByUsername(username);
 //		if(userFromDB!=null){
-		if(userRepo.findByUsername(username)!=null){
-			model.addAttribute("message","User already exists");
+		if (userRepo.findByUsername(username) != null) {
+			model.addAttribute("message", "User already exists");
 			return "registration";
-		} else{
-			User user = new User(username,password);
+		} else {
+			User user = new User(username, password);
 			user.setActive(true);
 			user.setRoles(Collections.singleton(Role.USER));
+			user.setActivationCode(UUID.randomUUID().toString());
+			if (!StringUtils.isEmpty(user.getEmail())) {
+				String message = String.format(
+						"Hello, %s!\n " +
+								"Welcome to Sweater. \n" +
+								"Please visit next link http://localhost/activate/%s",
+						user.getUsername(),
+						user.getActivationCode()
+				                              );
+				mailSender.send(user.getEmail(),"Activation code", message);
+			}
+			
 			userRepo.save(user);
 		}
 		return "login";
 	}
-}
+	
+	@GetMapping("/activate/{code}")
+	public String activate(@PathVariable String code, Model model){
+		
+		boolean isActivated = userService.activateUser(code);
+		
+		return "login";
+	}
+	
+}//class end
--- src/main/java/org/nick/sweater/service/UserService.java
+++ src/main/java/org/nick/sweater/service/UserService.java
@@ -1,10 +1,14 @@
 package org.nick.sweater.service;
 
+import org.nick.sweater.domain.Role;
+import org.nick.sweater.domain.User;
 import org.nick.sweater.repos.UserRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -16,10 +20,36 @@ public class UserService implements UserDetailsService {
 	}
 	
 	
-	
-	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		return userRepo.findByUsername(username);
 	}
+	
+	public boolean activateUser(String code) {
+		User user = userRepo.findByActivationCode(code);
+		
+			if (user == null) {
+			return false;
+			}
+		
+		user.setActivationCode(null); //означает, что юзер подтвердил свой ящик, и удаляем код, как более не нужный
+		
+		userRepo.save(user);
+		
+		return true;
+	}
+
+//	public boolean addUser(User user){
+//		User userFromDb = userRepo.findByUsername(user.getUsername());
+//		if(userFromDb!=null){
+//			return false;
+//		}
+//
+//		user.setActive(true);
+//		user.setRoles(Collections.singleton(Role.USER));
+//		userRepo.save(user);
+//
+//		return true;
+//	}
+	
 }
